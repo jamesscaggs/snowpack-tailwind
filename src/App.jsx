c9d981a110f718a76a8028264a5535e7c36bdfb3
jamesscaggs <james.scaggs@gmail.com>
2020-11-10 04:25:24 -0600

    remove react ui in favor or tailwind-react-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './assets/css/tailwind.css';
+import React, { useState, useEffect } from "react";
+import "./assets/css/tailwind.css";
 
 function App() {
   // Create the count state.
