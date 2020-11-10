bfd583e2744107d171cb4ac8debf6966197f68d1
jamesscaggs <james.scaggs@gmail.com>
2020-11-09 18:53:12 -0600

    added react ui component library as a building block for me to build out the tailwind components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './assets/css/tailwind.css';
+import React from "react";
+import "./assets/css/tailwind.css";
+import { Button } from "react-ui";
+
+import { ThemeProvider } from "react-ui";
+import { tokens, components } from "react-ui/themes/light";
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-blue-900">Testing Snowpack!XX!!!</h1>
-      </header>
-    </div>
+    <ThemeProvider tokens={tokens} components={components}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-blue-900">Testing Snowpack!XX!!!</h1>
+        </header>
+        <Button className="ml-2">Testing</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
